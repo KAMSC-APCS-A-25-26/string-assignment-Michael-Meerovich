fe8a108457d7955ea9c4644d795a85a8146d05da
Michael-Meerovich <03michael.meerovich@kamsc.org>
2025-10-10 13:54:57 -0400

    Update PalindromeChecker.java

--- src/main/java/PalindromeChecker.java
+++ src/main/java/PalindromeChecker.java
@@ -4,9 +4,13 @@ public class PalindromeChecker {
     public static void main(String[] args) {
         // TODO: Create a Scanner and read a full line from the user with nextLine().
         //       Keep the original input string so you can print it in the final message.
+        Scanner sc = new Scanner(System.in);
+        String original, lowercase, reverse;
+        original = sc.nextLine();
 
         // TODO: Build a lowercase version of the input for checking (case-insensitive comparison).
         //       (You don't need to remove spaces/punctuation for the current tests.)
+
 
         // TODO: Determine whether the lowercase string reads the same forwards and backwards.
         //       Tip: compare it to a reversed copy of itself.
